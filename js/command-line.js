16b29c232b59933a4b4f153d06d460e47ba28e72
lukejans <91230392+lukejans@users.noreply.github.com>
2023-04-20 22:40:56 -0400

    Command line implementation v1

--- js/command-line.js
+++ js/command-line.js
@@ -1,18 +1,45 @@
 /* 
     COMMAND LINE
 
-    display commands from hidden textarea in the
-    command line interface (cli)
+    display input from hidden textarea in the
+    command line interface (cli).
 */
 const CLI = document.querySelector('.command-line');
 const INPUT = document.getElementById('type');
+const CARET = document.querySelector('.caret');
 // focus on textarea (INPUT)
 window.addEventListener('click', function (event) {
   if (event.target !== INPUT) {
     INPUT.focus();
   }
 });
-// function to run onInput
+// function to run on input from textarea
 function cliDisplayInput() {
   CLI.textContent = INPUT.value;
 }
+/*
+    CARET
+
+    terminal caret movements
+*/
+CARET.style.left = '0px';
+let movesRight = 0;
+let movesLeft;
+// change caret location based on arrow press, delete and type
+window.addEventListener('keydown', function (e) {
+  if (e.key === 'ArrowUp') {
+    e.preventDefault();
+  } else if (e.key === 'ArrowDown') {
+    e.preventDefault();
+  } else if (e.key === 'Enter') {
+    e.preventDefault();
+  } else if (e.key === 'ArrowLeft' && movesLeft !== 0) {
+    movesRight++;
+    movesLeft = INPUT.value.length - movesRight;
+    CARET.style.left = parseInt(CARET.style.left) - 9 + 'px';
+  } else if (e.key === 'ArrowRight' && movesRight !== 0) {
+    movesRight--;
+    movesLeft = INPUT.value.length - movesRight;
+    CARET.style.left = parseInt(CARET.style.left) + 9 + 'px';
+  }
+});
