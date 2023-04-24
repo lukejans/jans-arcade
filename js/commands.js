96aad2313fc9deac8f2c3671c1eb835ccac0adb7
lukejans <91230392+lukejans@users.noreply.github.com>
2023-04-24 00:45:51 -0400

    Added help and about commands

--- js/commands.js
+++ js/commands.js
@@ -1,4 +1,9 @@
-let banner = [
+/* 
+    COMMANDS
+    
+    all commands output
+*/
+const banner = [
   '              ___',
   '            ,"---".',
   '            :     ;',
@@ -21,7 +26,32 @@ let banner = [
   '<span class="text">Welcome to my interactive arcade terminal.</span>',
   '<span class="text">Type <span class="cmds">[help]</span> for a list of available commands.</span>',
 ];
+const help = [
+  '<br>',
+  '<span class="cmds">about</span>         project description',
+  '<span class="cmds">help</span>          list of available commands',
+  '<span class="cmds">banner</span>        print arcade banner',
+  '<span class="cmds">social</span>        links to social media accounts',
+  '<span class="cmds">clear</span>         clear screen & command history',
+  '<span class="cmds">history</span>       display command history',
+  '<br>',
+];
 
+const about = [
+  '<br>',
+  '<span class="alt">Alpha Build</span>',
+  "Terminal themed arcade to showcase various games I've ",
+  'built while learning javascript via <a>The Odin Project</a>.',
+  'This is an early stage version of the terminal.',
+  '<br>',
+];
+
+// list of commands
+const commandList = ['banner', 'help', 'about', ''];
+// check input validity
+function isValidCommand(textInput) {
+  return commandList.includes(textInput);
+}
 // CONSOLE ART
 console.log(
   `       __                    ___                        __ \n      / /___ _____  _____   /   |  ______________  ____/ /__    \n __  / / __ \\/ __ \\/ ___/  / /| | / ___/ ___/ __ \\/ __  / _ \\ \n/ /_/ / /_/ / / / (__  )  / ___ |/ /  / /__/ /_/ / /_/ /  __/ \n\\____/\\__,_/_/ /_/____/  /_/  |_/_/   \\___/\\__/_/\\__,_/\\___/ `
