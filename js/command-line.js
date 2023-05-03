94a123b8b1f87df3cca5ae37df575b2d21c1cd2c
lukejans <91230392+lukejans@users.noreply.github.com>
2023-05-03 02:04:57 -0400

    Get time f() to print with prompt

--- js/command-line.js
+++ js/command-line.js
@@ -181,10 +181,24 @@ function newTab(link) {
     window.open(link, '_blank');
   }, 500);
 }
+// get current time
+function getCurrentTime() {
+  const time = new Date();
+  return (
+    String(time.getHours()).padStart(2, '0') +
+    ':' +
+    String(time.getMinutes()).padStart(2, '0') +
+    ':' +
+    String(time.getSeconds()).padStart(2, '0')
+  );
+}
 // print prompt on output
 function printPrompt(cmd) {
   let prompt = document.createElement('p');
-  prompt.innerHTML = `<span>guest</span><span class="alt">@</span><span class="cmds">jansarcade</span><span class="alt">: ~ $ <span class="${isValidCommand(
+  prompt.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
+  <span class="time">${getCurrentTime()}</span> 
+  <br> 
+  <span>guest</span><span class="alt">@</span><span class="cmds">jansarcade</span><span class="alt">: ~ $ <span class="${isValidCommand(
     INPUT.value.trim()
   )}">${cmd}</span></span>`;
   prompt.className = 'prompt';
