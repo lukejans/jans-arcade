fca02452c80c12767eda704e89bf250624def18c
Luke Janssen <91230392+lukejans@users.noreply.github.com>
2024-01-24 10:15:18 -0500

    Remove clock in prompt print

--- src/terminal/command-line.js
+++ src/terminal/command-line.js
@@ -21,7 +21,7 @@ let before = document.getElementById('before'); // changes on clear cmd
 const CLI = document.querySelector('.command-line');
 const INPUT = document.getElementById('type');
 const CARET = document.querySelector('.caret');
-const TERMINAL = document.querySelector('.terminal');
+const TERMINAL = document.querySelector('.terminal-output');
 const TICK = document.querySelector('.tick');
 
 // focus on textarea (INPUT)
@@ -280,9 +280,7 @@ setInterval(updateClock, 1000);
 // print prompt on output
 function printPrompt(cmd) {
   let prompt = document.createElement('p');
-  prompt.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z"/></svg>
-  <span class="time">${getCurrentTime()}</span> 
-  <br> 
+  prompt.innerHTML = `
   <span>guest</span><span class="alt">@</span><span class="cmds">jansarcade</span>
   <span class="alt">  
   <span class="${isValidCommand(INPUT.value.trim())}">\> </span>
